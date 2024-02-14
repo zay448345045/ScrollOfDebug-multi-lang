Zrp200/ScrollOfDebug

Restructured to provide android support
* PackageTrie moved to SPD-CLASSES
* added a method to PlatformSupport to get classes, and implemented it in AndroidPlatformSupport

core/src/main/java/com/zrp200/scrollofdebug/ScrollOfDebug.java
@@ -604,7 +604,7 @@ public class ScrollOfDebug extends Scroll {
     public static PackageTrie trie = null; // loaded when needed.
     static {
         try {
-            trie = PackageTrie.getClassesForPackage(ROOT);
+            trie = ShatteredPixelDungeon.platform.findClasses(ROOT);
         } catch (ClassNotFoundException e) { ShatteredPixelDungeon.reportException(e); }
     }
 

zay448345045/ScrollOfDebug-multi-lang

core/src/main/java/com/zrp200/scrollofdebug/ScrollOfDebug.java
@@ -1045,7 +1045,7 @@ public class ScrollOfDebug extends Scroll {
     public static PackageTrie trie = null; // loaded when needed.
     static {
         try {
-            trie = PackageTrie.getClassesForPackage(ROOT);
+            trie = Game.platform.findClasses(ROOT);
         } catch (ClassNotFoundException e) { Game.reportException(e); }
     }
 